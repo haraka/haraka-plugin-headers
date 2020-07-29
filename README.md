de21cfa2d7a02b1b69538604302fb5529ab85964
Matt Simerson <matt@tnpi.net>
2020-07-28 18:16:34 -0700

    imported from Haraka, working test, GitHub workflows

--- README.md
+++ README.md
@@ -1,63 +1,18 @@
 [![Build Status][ci-img]][ci-url]
+[![Windows Build Status][ci-win-img]][ci-win-url]
 [![Code Climate][clim-img]][clim-url]
+
 [![NPM][npm-img]][npm-url]
 
-# haraka-plugin-haraka-plugin-headers
+# haraka-plugin-headers
 
-Clone me, to create a new plugin!
-
-# Template Instructions
-
-These instructions will not self-destruct after use. Use and destroy.
-
-See also, [How to Write a Plugin](https://github.com/haraka/Haraka/wiki/Write-a-Plugin) and [Plugins.md](https://github.com/haraka/Haraka/blob/master/docs/Plugins.md) for additional plugin writing information.
-
-## Create a new repo for your plugin
-
-Haraka plugins are named like `haraka-plugin-something`. All the namespace after `haraka-plugin-` is yours for the taking. Please check the [Plugins]() page and a Google search to see what plugins already exist.
-
-Once you've settled on a name, create the GitHub repo. On the repo's main page, click the _Clone or download_ button and copy the URL. Then paste that URL into a local ENV variable with a command like this:
-
-```sh
-export MY_GITHUB_ORG=haraka
-export MY_PLUGIN_NAME=haraka-plugin-SOMETHING
-```
-
-Clone and rename the haraka-plugin-headers repo:
-
-```sh
-git clone git@github.com:haraka/haraka-plugin-haraka-plugin-headers.git
-mv haraka-plugin-haraka-plugin-headers $MY_PLUGIN_NAME
-cd $MY_PLUGIN_NAME
-git remote rm origin
-git remote add origin "git@github.com:$MY_GITHUB_ORG/$MY_PLUGIN_NAME.git"
-```
-
-Now you'll have a local git repo to begin authoring your plugin
-
-## rename boilerplate
-
-Replaces all uses of the word `haraka-plugin-headers` with your plugin's name.
-
-./redress.sh [something]
-
-You'll then be prompted to update package.json and then force push this repo onto the GitHub repo you've created earlier.
-
-
-## Enable Travis-CI testing
-
-- [ ] visit your [Travis-CI profile page](https://travis-ci.org/profile) and enable Continuous Integration testing on the repo
-- [ ] enable Code Climate. Click the _code climate_ badge and import your repo.
-
-
-
-# Add your content here
+This plugin performs a variety of mail header inspections.
 
 ## INSTALL
 
 ```sh
 cd /path/to/local/haraka
-npm install haraka-plugin-haraka-plugin-headers
+npm install haraka-plugin-headers
 echo "haraka-plugin-headers" >> config/plugins
 service haraka restart
 ```
@@ -67,19 +22,111 @@ service haraka restart
 If the default configuration is not sufficient, copy the config file from the distribution into your haraka config dir and then modify it:
 
 ```sh
-cp node_modules/haraka-plugin-haraka-plugin-headers/config/haraka-plugin-headers.ini config/haraka-plugin-headers.ini
-$EDITOR config/haraka-plugin-headers.ini
+cp node_modules/haraka-plugin-headers/config/headers.ini config/headers.ini
+$EDITOR config/headers.ini
 ```
 
-## USAGE
+## RFC 5322 Section 3.6:
+
+    All messages MUST have a 'Date' and 'From' header and a message may not contain more than one 'Date', 'From', 'Sender', 'Reply-To', 'To', 'Cc', 'Bcc', 'Message-Id', 'In-Reply-To', 'References' or 'Subject' header.
+
+The next two tests encompass the RFC 5322 checks:
+
+## duplicate\_singular
+
+Assure that all the singular headers are present only once. The list of
+headers can be adjusted in config/data.headers.ini:
+
+    * singular=Date,From,Sender,Reply-To,To,Cc,Bcc,Message-Id,In-Reply-To,References,Subject
+
+## missing\_required
+
+Assuring that all the required headers are present. The list of required
+headers can be altered in config/data.headers.ini:
+
+    required=From,Date
+
+## invalid\_return\_path
+
+Messages arriving via the internet should not have a Return-Path header set.
+This checks for that header (unless connection.relaying is set).
+
+## invalid\_date
+
+Checks the date header and makes sure it's somewhat sane. By default, the date
+cannot be more than 2 days in the future nor 15 days in the past. These can be
+adjusted in config/data.headers.ini:
+
+    date_future_days=2
+    date_past_days=15
+
+## user\_agent
+
+Attempt to determine the User-Agent that generated the email. A UA is
+determinable on about 70% of hammy messages.
+
+## direct\_to\_mx
+
+Counts the received headers. If there aren't at least two, then the MUA is
+attempting direct delivery to us instead of via their outbound SMTP server.
+This is typical of spam, our own users sending outbound email (which bypasses
+this test), and machine generated messages like Facebook/Twitter
+notifications.
+
+## from\_match
+
+See if the header From domain matches the envelope FROM domain. There are many
+legit reasons to not match, but matching domains are far more frequent in ham.
+
+## mailing\_list
+
+Attempt to determine if this message was sent via an email list. This is very
+rudimentary at present and only detects the most common email lists.
+
+Forwarders, of which email lists are a special type, constitutes the majority
+of the minority (~10%) of ham which fails SPF and DKIM tests. This MLM
+detector is a building block in the ability to detect mail from forwarders
+and assess their reputability.
+
+# Configuration
+
+The headers.ini file can contain [check] and [reject] sections.
+
+## [check]
+
+To turn on User Agent detection and turn off Mailing List detection:
+Each key is the test/check name and a boolean value that enables or disables the check.
+
+```ini
+[check]
+duplicate_singular=true
+missing_required=true
+invalid_return_path=true
+invalid_date=true
+user_agent=true
+direct_to_mx=true
+from_match=true
+mailing_list=true
+```
+
+## [reject]
+
+Turning off reject for a check lets it be enabled (for data collection)
+without interrupting mail flow. To prevent a missing header from causing
+messages to be rejected:
+
+```ini
+[reject]
+missing_required=false
+```
 
 
 <!-- leave these buried at the bottom of the document -->
-[ci-img]: https://travis-ci.org/haraka/haraka-plugin-haraka-plugin-headers.svg
-[ci-url]: https://travis-ci.org/haraka/haraka-plugin-haraka-plugin-headers
-[ci-win-img]: https://ci.appveyor.com/api/projects/status/CHANGETHIS?svg=true
-[ci-win-url]: https://ci.appveyor.com/project/haraka/haraka-CHANGETHIS
-[clim-img]: https://codeclimate.com/github/haraka/haraka-plugin-haraka-plugin-headers/badges/gpa.svg
-[clim-url]: https://codeclimate.com/github/haraka/haraka-plugin-haraka-plugin-headers
-[npm-img]: https://nodei.co/npm/haraka-plugin-haraka-plugin-headers.png
-[npm-url]: https://www.npmjs.com/package/haraka-plugin-haraka-plugin-headers
+[ci-img]: https://github.com/haraka/haraka-plugin-headers/workflows/Plugin%20Tests/badge.svg
+[ci-url]: https://github.com/haraka/haraka-plugin-headers/actions?query=workflow%3A%22Plugin+Tests%22
+[ci-win-img]: https://github.com/haraka/haraka-plugin-headers/workflows/Plugin%20Tests%20-%20Windows/badge.svg
+[ci-win-url]: https://github.com/haraka/haraka-plugin-headers/actions?query=workflow%3A%22Plugin+Tests+-+Windows%22
+[clim-img]: https://codeclimate.com/github/haraka/haraka-plugin-headers/badges/gpa.svg
+[clim-url]: https://codeclimate.com/github/haraka/haraka-plugin-headers
+[npm-img]: https://nodei.co/npm/haraka-plugin-headers.png
+[npm-url]: https://www.npmjs.com/package/haraka-plugin-headers
--- test/index.js
+++ test/index.js
@@ -3,6 +3,7 @@
 const assert   = require('assert');
 
 // npm modules
+const Address  = require('address-rfc2821');
 const fixtures = require('haraka-test-fixtures');
 
 // start of tests
@@ -10,27 +11,320 @@ const fixtures = require('haraka-test-fixtures');
 //    mocha: http://mochajs.org
 
 beforeEach(function (done) {
-    this.plugin = new fixtures.plugin('haraka-plugin-headers');
-    done();  // if a test hangs, assure you called done()
-});
+  this.plugin = new fixtures.plugin('haraka-plugin-headers')
+  this.plugin.register()
+
+  try {
+    this.plugin.addrparser = require('address-rfc2822');
+  }
+  catch (ignore) {}
+
+  this.connection = fixtures.connection.createConnection();
+  this.connection.transaction = fixtures.transaction.createTransaction();
+
+  done()  // if a test hangs, assure you called done()
+})
 
 describe('haraka-plugin-headers', function () {
-    it('loads', function (done) {
-        assert.ok(this.plugin);
-        done();
-    });
-});
+  it('loads', function (done) {
+    assert.ok(this.plugin)
+    done();
+  })
+})
 
-describe('load_haraka-plugin-headers_ini', function () {
-    it('loads haraka-plugin-headers.ini from config/haraka-plugin-headers.ini', function (done) {
-        this.plugin.load_haraka-plugin-headers_ini();
-        assert.ok(this.plugin.cfg);
-        done();
-    });
+describe('load_headers_ini', function () {
+  it('loads headers.ini from config/headers.ini', function (done) {
+    this.plugin.load_headers_ini()
+    // console.log(this.plugin.cfg);
+    assert.ok(this.plugin.cfg)
+    done()
+  })
 
-    it('initializes enabled boolean', function (done) {
-        this.plugin.load_haraka-plugin-headers_ini();
-        assert.equal(this.plugin.cfg.main.enabled, true, this.plugin.cfg);
-        done();
-    });
-});
+  it('initializes enabled boolean', function (done) {
+    this.plugin.load_headers_ini()
+    assert.equal(this.plugin.cfg.check.duplicate_singular, true)
+    done()
+  })
+})
+
+
+describe('user_agent', function () {
+
+  it('none', function (done) {
+    const outer = this;
+    this.plugin.cfg.check.user_agent=true;
+    this.plugin.user_agent(function () {
+      const r = outer.connection.transaction.results.get('haraka-plugin-headers');
+      assert.equal(/UA/.test(r.fail), true);
+      assert.equal(/UA/.test(r.pass), false);
+      done()
+    }, this.connection);
+  })
+
+  it('thunderbird', function (done) {
+    const outer = this
+    outer.plugin.cfg.check.user_agent=true
+    outer.connection.transaction.header.add_end('User-Agent', 'Thunderbird')
+    outer.plugin.user_agent(function () {
+      const r = outer.connection.transaction.results.get('haraka-plugin-headers')
+      // console.log(r)
+      assert.equal(true, /UA/.test(r.pass))
+      assert.equal(false, /UA/.test(r.fail))
+      done()
+    }, outer.connection)
+  })
+
+  it('X-mailer', function (done) {
+    const outer = this
+    outer.plugin.cfg.check.user_agent=true
+    outer.connection.transaction.header.add_end('X-Mailer', 'Apple Mail');
+    outer.plugin.user_agent(function () {
+      const r = outer.connection.transaction.results.get('haraka-plugin-headers');
+      assert.equal(true, /UA/.test(r.pass))
+      assert.equal(false, /UA/.test(r.fail))
+      done()
+    }, outer.connection);
+  })
+})
+
+describe('direct_to_mx', function () {
+
+  it('auth user', function (done) {
+    this.connection.notes.auth_user = 'test@example.com';
+    const outer = this;
+    this.plugin.cfg.check.direct_to_mx=true;
+    this.plugin.direct_to_mx(function () {
+      const r = outer.connection.transaction.results.get('haraka-plugin-headers');
+      assert.equal(true, /^direct-to-mx/.test(r.skip));
+      assert.equal(false, /^direct-to-mx/.test(r.pass));
+      assert.equal(false, /^direct-to-mx/.test(r.fail));
+      done()
+    }, this.connection);
+  })
+
+  it('received 0', function (done) {
+    const outer = this;
+    this.plugin.cfg.check.direct_to_mx=true;
+    this.plugin.direct_to_mx(function () {
+      const r = outer.connection.transaction.results.get('haraka-plugin-headers');
+      assert.equal(true, /^direct-to-mx/.test(r.fail));
+      assert.equal(false, /^direct-to-mx/.test(r.pass));
+      assert.equal(false, /^direct-to-mx/.test(r.skip));
+    }, this.connection);
+    done()
+  })
+  it('received 1', function (done) {
+    const outer = this;
+    this.plugin.cfg.check.direct_to_mx=true;
+    this.connection.transaction.header.add_end('Received', 'blah');
+    this.plugin.direct_to_mx(function () {
+      const r = outer.connection.transaction.results.get('haraka-plugin-headers');
+      assert.equal(true, /^direct-to-mx/.test(r.fail));
+    }, this.connection);
+    done()
+  })
+  it('received 2', function (done) {
+    const outer = this;
+    this.plugin.cfg.check.direct_to_mx=true;
+    this.connection.transaction.header.add_end('Received', 'blah1');
+    this.connection.transaction.header.add_end('Received', 'blah2');
+    this.plugin.direct_to_mx(function () {
+      const r = outer.connection.transaction.results.get('haraka-plugin-headers');
+      assert.equal(true, /^direct-to-mx/.test(r.pass));
+      assert.equal(false, /^direct-to-mx/.test(r.fail));
+      assert.equal(false, /^direct-to-mx/.test(r.skip));
+      done()
+    }, this.connection);
+  })
+})
+
+describe('from_match', function () {
+
+  it('match bare', function (done) {
+    const outer = this;
+    this.plugin.cfg.check.from_match=true;
+    this.connection.transaction.mail_from = new Address.Address('<test@example.com>');
+    this.connection.transaction.header.add_end('From', 'test@example.com');
+    this.plugin.from_match(function () {
+      const r = outer.connection.transaction.results.get('haraka-plugin-headers');
+      assert.notEqual(-1, r.pass.indexOf('from_match'));
+      done()
+    }, this.connection)
+  })
+  it('match typical', function (done) {
+    const outer = this;
+    this.plugin.cfg.check.from_match=true;
+    this.connection.transaction.mail_from = new Address.Address('<test@example.com>');
+    this.connection.transaction.header.add_end('From', '"Test User" <test@example.com>');
+    this.plugin.from_match(function () {
+      const r = outer.connection.transaction.results.get('haraka-plugin-headers');
+      assert.notEqual(-1, r.pass.indexOf('from_match'));
+      done()
+    }, outer.connection);
+  })
+  it('match unquoted', function (done) {
+    const outer = this;
+    this.plugin.cfg.check.from_match=true;
+    this.connection.transaction.mail_from = new Address.Address('<test@example.com>');
+    this.connection.transaction.header.add_end('From', 'Test User <test@example.com>');
+    this.plugin.from_match(function () {
+      const r = outer.connection.transaction.results.get('haraka-plugin-headers');
+      assert.notEqual(-1, r.pass.indexOf('from_match'));
+      done()
+    }, this.connection);
+  })
+
+  it('mismatch', function (done) {
+    const outer = this;
+    this.plugin.cfg.check.from_match=true;
+    this.connection.transaction.mail_from = new Address.Address('<test@example.com>');
+    this.connection.transaction.header.add_end('From', "test@example.net");
+    // console.log(this.connection.transaction.results);
+    this.plugin.from_match(function () {
+      const r = outer.connection.transaction.results.get('haraka-plugin-headers');
+      assert.equal(true, /^from_match/.test(r.fail));
+      done()
+    }, this.connection);
+  })
+})
+
+describe('mailing_list', function () {
+
+  it('ezmlm true', function (done) {
+    const outer = this;
+    function next_cb () {
+      const r = outer.connection.transaction.results.get('haraka-plugin-headers');
+      assert.equal(true, /ezmlm/.test(r.pass));
+      assert.equal(0, r.fail.length);
+    }
+    this.plugin.cfg.check.mailing_list=true;
+    this.connection.transaction.header.add_end('Mailing-List', "blah blah: run by ezmlm");
+    this.plugin.mailing_list(next_cb, this.connection);
+    done()
+  })
+  it('ezmlm false', function (done) {
+    const outer = this;
+    function next_cb () {
+      const r = outer.connection.transaction.results.get('haraka-plugin-headers');
+      assert.equal(r.pass.length, 0);
+      assert.equal(true, /not/.test(r.msg));
+    }
+    this.plugin.cfg.check.mailing_list=true;
+    this.connection.transaction.header.add_end('Mailing-List', "blah blah random header tokens");
+    this.plugin.mailing_list(next_cb, this.connection);
+    done()
+  })
+  it('yahoogroups', function (done) {
+    const outer = this;
+    function next_cb () {
+      const r = outer.connection.transaction.results.get('haraka-plugin-headers');
+      assert.equal(true, /yahoogroups/.test(r.pass));
+    }
+    this.plugin.cfg.check.mailing_list=true;
+    outer.connection.transaction.header.add_end('Mailing-List', "blah blah such-and-such@yahoogroups.com email list");
+    this.plugin.mailing_list(next_cb, this.connection);
+    done()
+  })
+  it('majordomo', function (done) {
+    const outer = this;
+    function next_cb () {
+      const r = outer.connection.transaction.results.get('haraka-plugin-headers');
+      assert.equal(true, /majordomo/.test(r.pass));
+    }
+    this.plugin.cfg.check.mailing_list=true;
+    outer.connection.transaction.header.add_end('Sender', "owner-blah-blah whatcha");
+    outer.plugin.mailing_list(next_cb, outer.connection);
+    done()
+  })
+  it('mailman', function (done) {
+    const outer = this;
+    outer.connection.transaction.header.add_end('X-Mailman-Version', "owner-blah-blah whatcha");
+    function next_cb () {
+      const r = outer.connection.transaction.results.get('haraka-plugin-headers');
+      assert.equal(true, /mailman/.test(r.pass));
+    }
+    this.plugin.cfg.check.mailing_list=true;
+    this.plugin.mailing_list(next_cb, this.connection);
+    done()
+  })
+  it('majordomo v', function (done) {
+    const outer = this;
+    function next_cb () {
+      const r = outer.connection.transaction.results.get('haraka-plugin-headers');
+      assert.equal(true, /majordomo/.test(r.pass));
+    }
+    this.plugin.cfg.check.mailing_list=true;
+    this.connection.transaction.header.add_end('X-Majordomo-Version', "owner-blah-blah whatcha");
+    this.plugin.mailing_list(next_cb, this.connection);
+    done()
+  })
+  it('google groups', function (done) {
+    const outer = this;
+    function next_cb () {
+      const r = outer.connection.transaction.results.get('haraka-plugin-headers');
+      assert.equal(true, /googlegroups/.test(r.pass));
+    }
+    this.plugin.cfg.check.mailing_list=true;
+    this.connection.transaction.header.add_end('X-Google-Loop', "blah-blah whatcha");
+    this.plugin.mailing_list(next_cb, this.connection);
+    done()
+  })
+})
+
+describe('delivered_to', function () {
+
+  it('disabled', function (done) {
+    const next_cb = function (res, msg) {
+      assert.equal(undefined, res);
+      assert.equal(undefined, msg);
+      done()
+    }.bind(this);
+    this.plugin.cfg.check.delivered_to=false;
+    this.plugin.delivered_to(next_cb, this.connection);
+  })
+  it('header not present', function (done) {
+    const next_cb = function (res, msg) {
+      assert.equal(undefined, res);
+      assert.equal(undefined, msg);
+      done()
+    }.bind(this);
+    this.plugin.cfg.check.delivered_to=true;
+    this.plugin.delivered_to(next_cb, this.connection);
+  })
+  it('no recipient match', function (done) {
+    const next_cb = function (res, msg) {
+      assert.equal(undefined, res);
+      assert.equal(undefined, msg);
+      done()
+    }.bind(this);
+    this.plugin.cfg.check.delivered_to=true;
+    // this.connection.transaction.mail_from = new Address.Address('<test@example.com>');
+    this.connection.transaction.header.add_end('Delivered-To', "user@example.com");
+    this.plugin.delivered_to(next_cb, this.connection);
+  })
+  it('recipient match', function (done) {
+    const next_cb = function (res, msg) {
+      assert.equal(DENY, res);
+      assert.equal('Invalid Delivered-To header content', msg);
+      done()
+    }.bind(this);
+    this.plugin.cfg.check.delivered_to=true;
+    // this.connection.transaction.mail_from = new Address.Address('<test@example.com>');
+    this.connection.transaction.header.add_end('Delivered-To', "user@example.com");
+    this.connection.transaction.rcpt_to.push(new Address.Address('user@example.com'));
+    this.plugin.delivered_to(next_cb, this.connection);
+  })
+  it('recipient match, reject disabled', function (done) {
+    const next_cb = function (res, msg) {
+      assert.equal(undefined, res);
+      assert.equal(undefined, msg);
+      done()
+    }.bind(this);
+    this.plugin.cfg.check.delivered_to=true;
+    this.plugin.cfg.reject.delivered_to=false;
+    // this.connection.transaction.mail_from = new Address.Address('<test@example.com>');
+    this.connection.transaction.header.add_end('Delivered-To', "user@example.com");
+    this.connection.transaction.rcpt_to.push(new Address.Address('user@example.com'));
+    this.plugin.delivered_to(next_cb, this.connection);
+  })
+})
